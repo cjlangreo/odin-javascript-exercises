24a2ea274c725adc630f7531930c78b44da7c9ca
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-17 00:12:41 +0800

    foundations-07 complete

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let finalString = "";
+  for(let i = 0; i < string.length; i++){
+    finalString += string.at(-i -1)
+  }
+  return finalString
 };
 
 // Do not edit below this line
