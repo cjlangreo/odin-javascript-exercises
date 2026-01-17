0345443f7f3078427cc2a5a330ba19f594708eb4
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-17 17:51:35 +0800

    foundations-10 complete

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if(typeof(year) == 'number' && year % 4 == 0){
+    if(year % 100 == 0 && year % 400 != 0){
+      return false
+    }
+    return true
+  }
+  return false
 };
 
 // Do not edit below this line
