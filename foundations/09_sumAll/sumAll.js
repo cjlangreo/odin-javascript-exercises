f11a63f5f4d082124fe92a40c7c4a0249cb32db8
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-17 17:31:55 +0800

    foundations-09 complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
 
+
+  if ([num1, num2].find(
+    num => num < 0 ||
+    typeof(num) != 'number' ||
+    num % 1 !== 0
+  )) return "ERROR"
+  
+  
+  if(num2 > num1){
+    for(let i = num1;i <= num2;i++){
+      sum += i;
+    }
+  } else if (num1 > num2){
+    for(let i = num2; i<= num1; i++){
+      sum += i;
+    }
+  } else return 0;
+
+  return sum
 };
+
+console.log(sumAll(-10, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
