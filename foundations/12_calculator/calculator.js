708bb3a756beeaa81317b9a93dfc9e77828c9968
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-17 18:22:55 +0800

    foundations-12 complete

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let sum = 0;
+  numArray.forEach(num => sum += num)
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let product = 1;
+  numArray.forEach(num => product *= num)
+  return product
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let powerProduct = 1;
+  for(let i = 0; i < num2; i++){
+    powerProduct *= num1
+  }
+  return powerProduct
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorialProduct = 1;
+  for(let i = num; i > 0; i--){
+    factorialProduct *= i
+  }
+  return factorialProduct
 };
 
 // Do not edit below this line
