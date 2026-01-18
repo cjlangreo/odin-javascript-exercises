fa0715b98479810eda6bddd484ff44f9e9077587
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-18 19:41:40 +0800

    foundations-14 wip

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  if(+index < 0) return "OOPS"
+
+  let previousNumber = 0;
+  let currentNumber = 1;
+
+  for(let i = 0; i < index; i++){
+    previousNumber = previousNumber + currentNumber, (previousNumber = currentNumber)   
+    console.log(`previous number: ${previousNumber}`)
+    console.log(`current number: ${currentNumber}`)
+  }
+
 
 };
 
