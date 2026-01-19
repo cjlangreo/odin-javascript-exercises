34d6c1518e50055dafafdc5bbcec71e2384cf5fe
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-19 18:35:48 +0800

    foundations-16 weird approach complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getOlder = (currentOldest, currentPerson) => {
+  const birth = currentPerson.yearOfBirth
+  const death = currentPerson.yearOfDeath ?? (new Date).getFullYear()
+  currentPerson.age = death - birth
 
+  
+  return currentPerson.age > currentOldest.age ? currentPerson : currentOldest
+}
+
+const findTheOldest = function(people) {
+  const oldestPerson = people.reduce(getOlder, {
+    name: "Base", yearOfBirth: 0, yearOfDeath: 0, age: 0
+  })
+  return oldestPerson
 };
 
 // Do not edit below this line
